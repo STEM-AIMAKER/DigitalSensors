f7432fcd1f141db2d482732ad3f6f3e7878b1a1f
stem-hanshin <59435476+stem-hanshin@users.noreply.github.com>
2020-12-07 10:42:50 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  * AIMaker STEM Sensors
  */
 //% color=190 weight=100 icon="\uf1ec" block="AIMaker: digital sensors"
-//% groups=['Shake Sensor','Water Pump','Fan', 'Button','LED','Reed Switch','Passive Infrared Sensor','Relay','Vibration Motor','others']
+//% groups=['Shake Sensor','Water Pump','Fan', 'Button','LED','Reed Switch','Passive Infrared Sensor','Relay','Vibration Motor','JoyStick','others']
 
 namespace aimakerdigitalsensors
 {
@@ -10,7 +10,7 @@ namespace aimakerdigitalsensors
  * AIMaker STEM Sensors
  */
 // color=190 weight=100 icon="\uf1ec" block="AIMaker: digital sensors"
-// groups=['Shake Sensor','Water Pump','Fan', 'Button','LED','Reed Switch','Passive Infrared Sensor','Relay','Vibration Motor','others']
+// groups=['Shake Sensor','Water Pump','Fan', 'Button','LED','Reed Switch','Passive Infrared Sensor','Relay','Vibration Motor','Joystick','others']
 
     //% group="Shake Sensor"
     export namespace Shake
@@ -305,4 +305,62 @@ namespace aimakerdigitalsensors
                 pins.digitalWritePin(motorPin, 0)
         }
     };
+     
+ //% group="Joystick"
+  export namespace JoyStick {
+    let kPin: DigitalPin = null
+    let xPin: AnalogPin = null
+    let yPin: AnalogPin = null
+    let onKPressedEventHandlerTrue: () => void;
+    let onKPressedEventHandlerFalse: () => void;
+    
+    //% blockId=readYValue block="Read JoyStick Y Value"
+    export function readYValue(): number {
+        return pins.analogReadPin(yPin)
+    }
+    
+    //% blockId=readXValue block="Read JoyStick X Value"
+    export function readXValue(): number {
+        return pins.analogReadPin(xPin)
+    }
+
+    //% blockId=isKPressed block="Is JoyStick K pressed"
+    export function isKPressed(): boolean {
+        if( 0 === pins.digitalReadPin(kPin) ) {
+            return true
+        }
+        return false;
+    }
+    
+    /**
+     * Registers code to run when k buton pressed.
+     */
+    //% blockId=onShakeEvent block="On K button at pin=%k pressed=%pressed event" 
+    export function onShakeEvent(k: DigitalPin,pressed: boolean, cb: () => void) {
+        kPin = k
+        if( pressed )
+            onKPressedEventHandlerTrue = cb
+        else
+            onKPressedEventHandlerFalse = cb
+        
+        pins.setPull(kPin, PinPullMode.PullNone)
+        
+        pins.onPulsed(kPin, PulseValue.High, function () {
+            if( onKPressedEventHandlerTrue )
+                onKPressedEventHandlerTrue()
+        })
+        pins.onPulsed(kPin, PulseValue.Low, function () {
+            if( onKPressedEventHandlerFalse )
+                onKPressedEventHandlerFalse()
+        })
+    }
+
+    //% blockId=connectJoyStick block="Connect JoyStick at K=%k|X=%x|y=%y"
+    export function connectJoyStick(k: DigitalPin, x: AnalogPin, y: AnalogPin): void {
+        kPin = k
+        xPin = x
+        yPin = y
+        
+    }
+  };
 }
